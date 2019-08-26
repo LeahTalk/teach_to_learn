578e12297bf139a3706eceae8b16f7c6a2faf7c2
Jonathan Lee <jonathan_496@hotmail.com>
2019-08-26 15:18:59 -0700

    fixed typo

--- apps/message_app/views.py
+++ apps/message_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render, HttpResponse, redirect
 from .models import *
 
 def index(request, appointment_id):
-    return render(request, 'message_app/messages.html')
+    return render(request, 'message_app/index.html')
 
